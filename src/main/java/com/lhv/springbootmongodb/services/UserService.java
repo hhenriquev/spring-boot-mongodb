73b5233bf14ffc0fe2da779df7deda6c5fceb045
Lucas H <lucas.henriquevio@gmail.com>
2024-01-24 18:37:44 -0300

    User insertion with POST

--- src/main/java/com/lhv/springbootmongodb/services/UserService.java
+++ src/main/java/com/lhv/springbootmongodb/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.lhv.springbootmongodb.domain.User;
+import com.lhv.springbootmongodb.dto.UserDTO;
 import com.lhv.springbootmongodb.repository.UserRepository;
 import com.lhv.springbootmongodb.services.exception.ObjectNotFoundException;
 
@@ -22,6 +23,14 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }
 
