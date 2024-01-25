35463757d376e2d2d8a91eac5b0bfd2449f69ce7
Lucas H <lucas.henriquevio@gmail.com>
2024-01-25 09:51:32 -0300

    Getting post by id

---
+++ src/main/java/com/lhv/springbootmongodb/services/PostService.java
@@ -0,0 +1,25 @@
+package com.lhv.springbootmongodb.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.lhv.springbootmongodb.domain.Post;
+import com.lhv.springbootmongodb.repository.PostRepository;
+import com.lhv.springbootmongodb.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired
+	private PostRepository repo;	
+
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+}
+
+
+
