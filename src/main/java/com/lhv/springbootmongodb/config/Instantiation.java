7cc629e2102c3b4694d9163e6ea786b2f4d08810
Lucas H <lucas.henriquevio@gmail.com>
2024-01-25 09:25:40 -0300

    Projection of author data with DTO

--- src/main/java/com/lhv/springbootmongodb/config/Instantiation.java
+++ src/main/java/com/lhv/springbootmongodb/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 
 import com.lhv.springbootmongodb.domain.Post;
 import com.lhv.springbootmongodb.domain.User;
+import com.lhv.springbootmongodb.dto.AuthorDTO;
 import com.lhv.springbootmongodb.repository.PostRepository;
 import com.lhv.springbootmongodb.repository.UserRepository;
 
@@ -35,10 +36,11 @@ public class Instantiation implements CommandLineRunner{
 		User alex = new User(null, "Alex Green", "alex@gmail.com");
 		User bob = new User(null, "Bob Grey", "bob@gmail.com");
 		
-		Post post1 = new Post(null, sdf.parse("21/01/2024"), "Partiu viajar", "Vou viajar para Londres. Abraços", maria);
-		Post post2 = new Post(null, sdf.parse("23/01/2024"), "Bom dia!", "Acordei bem hoje!", maria);
-		
 		userRepository.saveAll(Arrays.asList(maria, alex, bob));
+		
+		Post post1 = new Post(null, sdf.parse("21/01/2024"), "Partiu viajar", "Vou viajar para Londres. Abraços", new AuthorDTO(maria));
+		Post post2 = new Post(null, sdf.parse("23/01/2024"), "Bom dia!", "Acordei bem hoje!", new AuthorDTO(maria));
+				
 		postRepository.saveAll(Arrays.asList(post1, post2));
 	}
 
